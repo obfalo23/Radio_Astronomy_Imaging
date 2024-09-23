6442a13cb78cfa53fbf92556860f586e6a7efd82
timvanharen <timvanharen@live.com>
2024-09-23 11:20:27 +0200

    For loop dirty beam calculation
    DIkke koe op een driewieler + 200g boter

--- lofar_data/imaging_template.m
+++ lofar_data/imaging_template.m
@@ -18,6 +18,9 @@ clc
 
 load lofar_DSP_data_1.mat
 
+figure
+scatter(poslocal(:,1),poslocal(:,2))
+
 %% Initialization
 c = 299792458; % speed of light
 p = size(Rh,1);% number of receiving elements
@@ -26,8 +29,8 @@ lambda = c / freq; % wavelength at the observation frequency
 % define a distance matrix of all baselines (=vectors between pairs of antennas)
 dist = sqrt(  (meshgrid(poslocal(:,1)) - meshgrid(poslocal(:,1)).').^2 ...
             + (meshgrid(poslocal(:,2)) - meshgrid(poslocal(:,2)).').^2);
-D = max(dist(:)); % Maximum Baseline
 
+D = max(dist(:)); % Maximum Baseline
 
 %% Coordinate system
 % In astronomy it is common to use the (l,m,n) coordinates which 
@@ -39,7 +42,7 @@ D = max(dist(:)); % Maximum Baseline
 
 
 % define a grid of image coordinates (l,m) at the right resolution
-FracAngle = 0.25;% fraction of theoretical limit for angle resolution
+FracAngle = 4;% fraction of theoretical limit for angle resolution
                  % 0.25 means ~4x4 pixels in the main lobe
 dl = FracAngle * lambda / D; % width/height of a pixel
 l = 0:dl:1; % First image coordinate := cos(theta)cos(phi)
@@ -47,7 +50,39 @@ l = [-fliplr(l(2:end)),l]';
 m = l;  % Second image coordinate := cos(theta)sin(phi)
         % m = l means an aspect ratio 1:1
 
-
 %% Your Imaging Algorithm should come hereafter...
-image(poslocal)
-%test
+% Calculate dirty beam pattern
+
+% Define normalized location vector
+z = (pi*freq/c) * poslocal;
+
+
+% Calculate the dirty_beam per direction vector in for loops (slow)
+dirty_beam = ones(size(l,1), size(m,1))
+sum = 0
+for pos_l=1:size(l,1)
+    for pos_m=1:size(m,1)
+        n = sqrt(1 - l(pos_l)^2 - m(pos_m).^2);
+        direction_vector = [l(pos_l),m(pos_m),n];
+        for x = 1:p
+            for y = 1:p
+                baseline = (z(x,:)-z(y,:));
+                component = baseline*transpose(direction_vector);
+                sum = sum +  exp(1j*component);
+            end
+        end
+        dirty_beam(pos_l,pos_m) = sum;
+        sum = 0;
+    end
+    disp(pos_l)
+end
+
+% Plot dirty beam
+figure;
+imagesc(abs(dirty_beam));
+axis equal;        % Make axes equal for proper aspect ratio
+colormap('jet');   % Use the 'jet' colormap for colors
+colorbar;          % Display a color bar to the right
+% Set the color axis limits (optional, for consistent color scaling)
+caxis([0, 5000]); % 5000 looks cool
+
